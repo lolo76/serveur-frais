7fc92a2c422afc3ab4ab01a924fe060a021d137d
lolo <laurent.brelle@gmail.com>
2017-12-07 15:13:50 +0100

    motif

--- install-docker.sh
+++ install-docker.sh
@@ -1,15 +1,17 @@
-sudo apt-get remove -y docker docker-engine docker.io
-sudo apt-get update -y  
-sudo apt-get install -y \
+apt-get remove -y docker docker-engine docker.io
+apt-get update 
+apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
     software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository \
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+apt-key fingerprint 0EBFCD88
+add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-sudo apt-get update -y
-sudo apt-get install -y docker-ce
+apt-get update
+apt-get install -y docker-ce
+apt-get update
+apt-get dist-update
